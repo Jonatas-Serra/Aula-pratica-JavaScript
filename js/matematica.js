ed371c76afebb7b3103f58a05cf69136a8c8c863
Jonatas <jonatasserra@outlook.com>
2022-06-30 18:27:53 -0300

    Commit Inicial

---
+++ js/matematica.js
@@ -0,0 +1,16 @@
+class Matematica {
+    static somar(valor1, valor2) {
+        return valor1 + valor2
+    }
+    static multiplicar(valor1, valor2) {
+        return valor1 * valor2
+    }
+    static subtrair(valor1, valor2) {
+        return valor1 - valor2
+    }
+    static dividir(valor1, valor2) {
+        return valor1 / valor2
+    }
+}
+
+//Esse arquivo fica puvlico sozinho
